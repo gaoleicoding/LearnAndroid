9c619a41142520be59d50af32b63fb8a10456d88
GL <gl@GaoLeideMacBook-Pro.local>
2018-12-04 22:59:15 +0800

    删除多余代码

--- app/src/main/java/com/gaolei/famousfameproject/net/client/RestApiProviderBase.java
+++
@@ -1,8 +0,0 @@
-package com.gaolei.famousfameproject.net.client;
-
-/**
- * Created by liuhaiyang on 2016/9/6.
- */
-public interface RestApiProviderBase {
-    <T> T getApiService(Class<T> var1);
-}
--- app/src/main/java/com/gaolei/famousfameproject/net/url/UrlFactory.java
+++
@@ -1,9 +0,0 @@
-package com.gaolei.famousfameproject.net.url;
-
-
-/**
- * REST接口地址
- */
-public abstract class UrlFactory implements Cloneable {
-    public static final String BASE_URL = "http://www.weiyankeji.cn";
-}
